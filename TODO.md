b0f5b89c6aa2116d87a442b585e62e56bed0b04a
Stephen Rayner <stephen.r.rayner@gmail.com>
2025-06-08 19:50:32 +0100

    ci: setup RelativeCI

--- TODO.md
+++ TODO.md
@@ -15,3 +15,12 @@
 - [ ] Setup [Biome](https://biomejs.dev/), but check how it will work with Next.js, Turbo ESLint plugin
 - [ ] Ensure ESLint, Next.js, Turbo, Core web vitals, react-hooks are all working together nicely and reported in CI.
 - [ ] Convert the nextJS github action into a separate package.
+- [ ] Setup [autocomplete](https://www.sixian.li/writing/tailwind-autocomplete-done-right) [Tailwind Intellisense Regex List](https://github.com/paolotiu/tailwind-intellisense-regex-list) first saw it [here](https://github.com/Blazity/next-enterprise/blob/9743a37adfc54ce70f7e194f9ba3d505d56ad193/.vscode/settings.json)
+- [ ] Setup [Storybook](https://storybook.js.org/) [this](https://github.com/Blazity/next-enterprise/tree/9743a37adfc54ce70f7e194f9ba3d505d56ad193/.storybook) might be useful.
+- [ ] Look into [RelativeCI](https://relative-ci.com/)
+
+<https://relative-ci.com/documentation/guides/bundle-stats/nextjs-webpack-stats>
+
+<https://relative-ci.com/documentation/setup/agent/github-action>
+
+So I can finish <https://app.relative-ci.com/projects/Nnwm15q7G8bjLYUwNHFm>
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,45 @@
+import { StatsWriterPlugin } from "webpack-stats-plugin";
+import filterWebpackStats from "@bundle-stats/plugin-webpack-filter";
+
+/**
+ * Configure webpack to generate stats for RelativeCI
+ * @param {import('webpack').Configuration} config - Webpack configuration
+ * @param {boolean} isServer - Whether this is a server-side build
+ * @returns {import('webpack').Configuration} Modified webpack configuration
+ */
+function configureRelativeCI(config, isServer) {
+	if (isServer || process.env.RELATIVE_CI !== "true") {
+		return config;
+	}
+
+	const statsPlugin = new StatsWriterPlugin({
+		// Output stats file to the project root (apps/web/) instead of .next/
+		filename: "../webpack-stats.json",
+		stats: {
+			assets: true,
+			chunks: true,
+			modules: true,
+		},
+		transform: (webpackStats) => {
+			// Filter out the stats that are not needed for RelativeCI
+			// https://relative-ci.com/documentation/guides/bundle-stats/nextjs-webpack-stats/#optimize-the-json-file-size
+			const filteredSource = filterWebpackStats(webpackStats);
+			return JSON.stringify(filteredSource);
+		},
+	});
+
+	return {
+		...config,
+		plugins: [...config.plugins, statsPlugin],
+	};
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	transpilePackages: ["@workspace/ui"],
+	webpack: (config, { isServer }) => {
+		return configureRelativeCI(config, isServer);
+	},
 };
 
 let exportConfig = nextConfig;
